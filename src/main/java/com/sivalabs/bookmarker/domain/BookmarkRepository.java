68e70238011b57415eebca77f7c337be9cafe94c
warrockhali <warrockhali@gmail.com>
2025-11-13 21:25:33 +0900

    Bookmark api 추가

---
+++ src/main/java/com/sivalabs/bookmarker/domain/BookmarkRepository.java
@@ -0,0 +1,7 @@
+package com.sivalabs.bookmarker.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+}
